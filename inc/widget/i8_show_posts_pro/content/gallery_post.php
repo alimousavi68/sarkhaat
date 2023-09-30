11eb9ac239965c0516c9dbf7306396e1d15acbfd
ali mousavi <seadali@gmail.com>
2023-09-30 08:35:05 +0330

    test commit- not real

--- inc/widget/i8_show_posts_pro/content/gallery_post.php
+++ inc/widget/i8_show_posts_pro/content/gallery_post.php
@@ -1,57 +1,35 @@
 <?php
 echo $args['before_widget'];
-echo '<div class="title-icon d-flex align-items-center mb-3">';
-echo '<div class="text-title box-title  ' . $head_font_size . ' fw-7 m-0 me-2">';
+echo '<div class="row box mx-0 d-flex row-gap-3 ">';
 if ($hide_title != 'on') {
+    echo '<div class="title-icon d-flex align-items-center mb-3">';
+    echo '<p class="text-title f24 fw-7 m-0 me-2">';
     echo $args['before_title'] . $icon_print . $title  .  $args['after_title'];
+    echo $sub_title_print . '</p></div>';
 }
-echo $sub_title_print . '</div></div>';
 
-// نمایش محتویات ویجت - نمایش پست ها
+// نمایش محتویات ویجت- نمایش پست ها
 $category_posts = new WP_Query(array(
     'posts_per_page' => $num,
     'cat'            => $cat,
     'order' => 'DESC',
-    'orderby' => $orderby
+    'orderby'=> $orderby
 ));
 
 if ($category_posts->have_posts()) {
     while ($category_posts->have_posts()) {
         $category_posts->the_post();
-
-        $primary_cat = get_post_meta(get_the_ID(), 'hasht_primary_category');
-        $primary_cat_color = get_term_meta($primary_cat[0], 'i8_CustomTerm_color', true) ? get_term_meta($primary_cat[0], 'i8_CustomTerm_color', true) : '#000000';
-        $primary_cat_color_transparent = $primary_cat_color . 'c4';
 ?>
-        <div class="<?php echo $col; ?> px-1">
-            <div class="post-flip-box-container">
-                <?php echo i8_the_thumbnail('i8-xl-625-486', 'post-flip-box-image object-fit-cover' . $thumb_radius, array("width" => $thumb_width, "height" => $thumb_height)); ?>
-
-                <div class="post-flip-box-link-wrapper">
-                    <a href="post1.html" class="post-flip-box-link"></a>
-                </div>
-                <div class="post-flip-box-overlay d-flex align-items-center">
-                    <h2 class="post-flip-box-title">
-                        <a class="<?php echo $title_font_size; ?> post-flip-box-link l1 text-white" href="<?php echo get_the_permalink(); ?>"><?php i8_limit_text(get_the_title(), 72, '...'); ?></a>
-                    </h2>
-                </div>
-            </div>
+        <div class="mini-article d-flex <?php echo $col; ?>  px-0 px-xl-2 px-lg-2 px-md-2 align-items-center">
+            <a href="<?php the_permalink(); ?>"><?php echo i8_the_thumbnail('i8-sm-85-67', 'hover', 67); ?></a>
+            <a class="f15 me-2 l22-05 text-grey" href="<?php echo get_the_permalink(); ?>"><?php i8_limit_text(get_the_title(), 72, '...'); ?></a>
         </div>
 <?php
     }
     wp_reset_postdata();
 }
-
-if ($hide_thumb != 'on') :
-    echo '</div>';
-else :
-    echo '</ul>';
-endif;
+echo '</div>';
 
 echo $args['after_widget'];
 
-
 ?>
-
-
-
