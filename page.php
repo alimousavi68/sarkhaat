069c743d9d219a8b33b3fa7375c1c501c0b58bda
ali mousavi <seadali@gmail.com>
2023-10-28 13:16:08 +0330

    update market data widget5

--- page.php
+++ page.php
@@ -11,7 +11,7 @@ get_header();
         <!-- sidebar  -->
         <div class="col-xl-7 col-md-24 col-sm-24 ps-0 pt-4 pt-xl-0 pt-md-4 pt-sm-4 pe-xl-3 pe-0 pe-sm-0 i8-sticky">
             <div class="box">
-                <?php dynamic_sidebar('hl-sidebar'); ?>
+                <?php dynamic_sidebar('al-sidebar'); ?>
             </div>
         </div>
 
